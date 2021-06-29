025a4a80269b7227bac84a0ef1edbcba73032863
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2021-06-29 15:55:03 -0700

    Another way

--- Palindrome.py
+++ Palindrome.py
@@ -1,3 +1,17 @@
+#One way : 44ms & 14.2 MB
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
+
+#second way : 98ms & 14.4 MB
+import math 
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        orig = x
+        back_x = 0
+        while x > 0:
+            back_x = (back_x * 10) + (x % 10)
+            x = x // 10
+        return orig == back_x
+
