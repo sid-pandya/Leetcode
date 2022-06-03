9faf0a6e58191f5816afca1b7bb4e1a5e87ff7d0
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-03 14:04:25 -0700

    2 Pointer Approach

--- Palindrome.py
+++ Palindrome.py
@@ -3,7 +3,28 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
 
-#second way : 98ms & 14.4 MB
+#Second Way : 79ms & 14.4 MB
+# Two pointer method
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        f = 0
+        l = len((s))-1
+        s = s.lower()
+        while f < l:
+            if not s[f].isalnum():
+                f += 1
+            elif not s[l].isalnum():
+                l -= 1
+            else:
+                if s[f] != s[l]:
+                    return False
+                else:
+                    f += 1
+                    l -= 1
+        return True
+    
+    
+#Third way : 98ms & 14.4 MB
 import math 
 
 class Solution:
