e1583f83339f3423be08c95bc8e453e5a6687c51
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-10 12:05:30 -0700

    Two pointer approach

--- reverse string.py
+++ reverse string.py
@@ -4,8 +4,11 @@ def reverseString(self, s: List[str]) -> None:
   """
   Do not return anything, modify s in-place instead.
   """
-  #two pointer method
+  #two pointer method : 199ms , 18 MB
   l ,r = 0, len(s)-1
   while l < r:
       s[l],s[r] = s[r],s[l]
       l,r = l+1, r-1
+     
+  #Pythonic solution : 300ms , 18.8 MB
+  s[:] = s[::-1]
