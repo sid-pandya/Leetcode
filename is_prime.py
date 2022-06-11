c8907d65c44bac90bbcfe8e371ddd1d866adb086
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-11 13:47:30 -0700

    Prime number

--- is_prime.py
+++ is_prime.py
@@ -1,1 +1,11 @@
+#Time: O(√n)
 
+def IsPrime(n):
+  if n < 2:
+    return False
+  if n < 4:
+    return True
+  for i in range(2,int(math.sqrt(n)+1)):
+    if n % i == 0:
+      return False
+  return True
