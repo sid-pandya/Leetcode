776221e556dd58d5e22de28fd00bdeea85ecf301
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-11 12:32:31 -0700

    Leetcode 392

--- Is Subsequence.py
+++ Is Subsequence.py
@@ -1,4 +1,23 @@
+**LeetCose 392**
 
+#36ms , 13.8 MB
+def isSubsequence(self, s: str, t: str) -> bool:
+    #check potential subsequence is smaller than main String
+    if len(s) > len(t):
+        return False
+    i=0
+    #loop for each char n main string
+    for item in t:
+        if len(s)==i:
+            return True
+        #compare the subsequence char with main string char
+        if s[i] == item:
+            i+=1
+    if i==len(s):
+        return True
+    return False
+    
+    
 #89ms, !4.1 MB
 def isSubsequence(self, s: str, t: str) -> bool:
     if len(s) > len(t):
