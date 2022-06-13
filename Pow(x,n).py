aa6e7eb2a509f22510f3ceaec7f46578f47d388c
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-12 18:41:27 -0700

    Divide into smaller parts

--- Pow(x,n).py
+++ Pow(x,n).py
@@ -1,7 +1,18 @@
 #LettCode-50 : Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
 
-
-
+#Time Complexity: O(logN) , Space Complexity: O(1)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n == 1:
+            return x
+        if n == -1:
+            return 1 / x
+        result = self.myPow(x, n // 2)
+        return result * result * (x if n % 2 else 1)
+    
+#Second approach
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         if n == 0:
