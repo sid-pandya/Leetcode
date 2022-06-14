f2609a8b2159cba280e5fc433975d8af58f08460
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-14 13:37:55 -0700

    LeetCode 347

--- Top K Freq Ele.py
+++ Top K Freq Ele.py
@@ -1,1 +1,20 @@
 #LeetCode -347 : Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+#183 ms , 18.7 MB
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+       
+        for element in nums:
+            if element not  in freq:
+                freq[element] = 1
+            else:
+                freq[element] += 1
+        freq = (sorted(freq.items(), key=lambda x: x[1], reverse=True))
+        
+        ret = []
+        for i in range(k):
+            ret.append(freq[i][0])
+            
+        return ret
+      
