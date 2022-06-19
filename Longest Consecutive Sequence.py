52af14389e2eda0921c3f072155ef0b53ada093f
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-19 12:33:42 -0700

    LeetCode 128

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -1,2 +1,38 @@
 # LeetCode 128 - Longest Consecutive Sequence
 Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence
+
+
+#sorting array approach :
+Time complexity: O(nLogn). 
+Auxiliary space : O(1).
+  
+```
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return 1
+        ans = 0
+        count = 0
+        
+        nums.sort()
+        
+        v = []
+        v.append(nums[0])
+        
+        for i in range(len(nums)):
+            if (nums[i] != nums[i-1]):
+                v.append(nums[i])
+                
+        for i in range(len(v)):
+            if (i > 0  and v[i] == v[i-1] + 1):
+                count +=1
+                
+            else:
+                count = 1
+            
+            ans = max(ans,count)
+        
+        return ans
+```
