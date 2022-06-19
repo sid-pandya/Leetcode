2dfc50af2d734b354fb3c60a25b05ee5ce655c7c
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-19 12:34:57 -0700

    LeetCode 128

--- Longest Consecutive Sequence.py
+++ Longest Consecutive Sequence.py
@@ -6,6 +6,29 @@ Given an unsorted array of integers nums, return the length of the longest conse
 Time complexity: O(nLogn). 
 Auxiliary space : O(1).
   
+  
+```
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        nums.sort()
+
+        longest_streak = 1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak += 1
+                else:
+                    longest_streak = max(longest_streak, current_streak)
+                    current_streak = 1
+
+        return max(longest_streak, current_streak)
+```
+
 ```
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
