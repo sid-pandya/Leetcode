0b69f487c3bf65e99901da1bd82532e3cfea7eaa
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-20 11:34:36 -0700

    LeetCode 169

--- Majority Element.py
+++ Majority Element.py
@@ -1,1 +1,14 @@
+Leetcode 169 - Given an array nums of size n, return the majority element.
 
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+
+#Sorting Approach
+#Time complexity : O(nlgn)
+#Space complexity : O(1) or O(n)
+```
+class Solution:
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)//2]
+```
