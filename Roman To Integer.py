c88bae9ccfc45c2ef60a01cd930a114532e5c728
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-21 11:58:25 -0700

    LeetCode 13

--- Roman To Integer.py
+++ Roman To Integer.py
@@ -1,5 +1,30 @@
 #LeetCode - 13 : Given a roman numeral, convert it to an integer.
 
+#using hashtable (Dictionary)
+
+
+```
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dict = {'I' : 1,
+                'V' : 5,
+                'X' : 10,
+                'L' : 50,
+                'C' : 100,
+                'D' : 500,
+                'M' : 1000
+               }
+        l = len(s)
+        res = dict[s[l-1]]
+        for i in range(l-2,-1,-1):
+            if dict[s[i]] >= dict[s[i+1]]:
+                res += dict[s[i]]
+            else:
+                res -= dict[s[i]]
+        return res
+```
+
+
 #using .replace()
 Time complexity – O(N)
 Auxiliary Space – O(1)
