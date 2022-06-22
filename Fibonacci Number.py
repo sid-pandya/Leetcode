e7a221af0f9899c15444fb0b95c140ad5621f5f5
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-22 09:28:15 -0700

    Leetcode 509

--- Fibonacci Number.py
+++ Fibonacci Number.py
@@ -1,1 +1,14 @@
 # LeetCode - 509 :
+
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        a = 0
+        b = 1
+        for i in range(n-1):
+            a, b = b, a+b
+        return b
