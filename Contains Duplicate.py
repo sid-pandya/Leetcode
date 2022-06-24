69ea3fc5b79227352db68b85dc88b5f516ac103f
sid-pandya <86027069+sid-pandya@users.noreply.github.com>
2022-06-24 14:32:02 -0700

    LeetCode 217

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,2 +1,15 @@
 # LeetCode - 217 : Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+# hash set approach
+Time complexity: O(N),
+Memory: O(N) 
+```
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        h = set()
+        for i in nums:
+            if i in h:
+                return True
+            h.add(i)
+        return False
+```
